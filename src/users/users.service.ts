34c1051d427f32000eb7f09fd993b28f0c87a13f
gabrieltor <dariogabrieltorres93@gmail.com>
2024-03-11 01:46:38 -0300

    user service complete

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,27 +1,31 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class UsersService {
+  private prisma = new PrismaClient();
   constructor(private readonly databaseService: DatabaseService) { }
+
   async create(createUserDto: Prisma.UserCreateInput) {
-    return 'This action adds a new user';
+    return await this.prisma.user.create({
+      data: createUserDto,
+    });
   }
 
   async findAll() {
-    return `This action returns all users`;
+    return await this.prisma.user.findMany()
   }
 
-  async findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    return await this.prisma.user.findUnique({ where: { id } })
   }
 
-  async update(id: number, updateUserDto: Prisma.UserUpdateInput) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: Prisma.UserUpdateInput) {
+    return await this.prisma.user.update({ where: { id }, data: updateUserDto })
   }
 
-  async remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    return this.prisma.user.delete({ where: { id } })
   }
 }
